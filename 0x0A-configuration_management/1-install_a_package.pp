ab25a62e3286dcd1f19e7d58b2deb5d86c46f6f4
KenNyagz <kennyagz681@gmail.com>
2024-01-19 18:12:00 +0300

    An aliased Commit to save time

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,15 @@
 # installing flask from pip3
 
-#class { 'python':
-#  version => 'system',
-#}
+class { 'python':
+  version => 'system',
+}
 
-#package { 'python3-pip':
-#  ensure => installed,
-#}
+package { 'python3-pip':
+  ensure => installed,
+}
 
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
