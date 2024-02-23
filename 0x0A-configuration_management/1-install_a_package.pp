0a9f317866eb0d1817146f984754caa8f2f9ea41
KenNyagz <kennyagz681@gmail.com>
2024-02-23 07:12:07 -0800

    flaska

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,12 @@
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+class flask_installation(
+  $flask_version = '2.1.0',
+){
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  exec { "pip3 install flask==${flask_version}":
+    require => Package['python3-pip'],
+    path => '/usr/bin:/usr/sbin:/bin:/sbin',
+  }
 }
