59fc64babfa184150e7e7286bfe66fd0eb7792a7
KenNyagz <kennyagz681@gmail.com>
2024-02-23 22:00:54 +0300

    An aliased Commit to save time

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,12 +5,9 @@ package { 'Flask':
   provider => 'gem',
 }
 
-<<<<<<< HEAD
 exec { 'print_versions':
   command => 'flask --version',
   path    => ['/usr/local/bin', '/usr/bin', '/bin'],
   logoutput => true,
   require => Package['Flask'],
 }
-=======
->>>>>>> 41f08e5e3eb5406b088bf62625ad75b66c7d7a2c
