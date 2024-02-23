6b9e2d5d90cb46f6979014aaf73e9254bff866e2
KenNyagz <kennyagz681@gmail.com>
2024-02-23 10:32:38 -0800

    Flask installation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,4 @@
-# installing flask from pip3
-
-exec { 'upgrade_werkzeug':
-  command     => '/usr/bin/pip3 install --upgrade werkzeug',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  refreshonly => true,
-}
-
-package { 'Flask':
-  ensure   => '2.0.3',
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Exec['upgrade_werkzeug'],
-}
-
-exec { 'print_versions':
-  command   => 'flask --version',
-  path      => '/usr/local/bin:/usr/bin:/bin',
-  logoutput => true,
-  require   => Package['Flask'],
 }
