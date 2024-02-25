b66fff66cec49c7d50016cfdee3d536339783bc8
KenNyagz <kennyagz681@gmail.com>
2024-02-25 20:50:42 +0300

    puppet ssh confign

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,11 @@
 # making change to a config file in a remote server using puppet via SSH
 
-# Manifest to configure SSH client
-
-# Install puppetlabs-sshkeys_core module
-class { 'sshkeys_core':
-  client_package_name => 'openssh-client',
-}
-
-# Configure SSH client
-ssh::client::config { 'default':
-  ensure             => present,
-  user               => $::ssh::params::user,
-  path               => $::ssh::params::config_file,
-  priority           => 10,
-  host               => '<remote_server>',
-  identity_file      => '/home/<your_username>/.ssh/school',
-  password_auth      => 'no',
-}
-
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+  content => "
+            # client config
+         host *
+          IdentifyFile ~/.ssh/school
+          PasswordAuthentication no
+          ",
+} 
