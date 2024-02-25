ef81d2464539cef4fc8a36eb2b0ab8a5359b6c47
KenNyagz <kennyagz681@gmail.com>
2024-02-25 20:44:17 +0300

    An aliased Commit to save time

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,20 @@
 # making change to a config file in a remote server using puppet via SSH
 
-file { '/etc/ssh/ssh_config.d/myconfig.conf':
-  ensure  => file,
-  mode    => '0600',
-  content => "Host *\n    PubkeyAuthentication yes\n    PasswordAuthentication no\n    IdentityFile ~/.ssh/school\n",
+# Manifest to configure SSH client
+
+# Install puppetlabs-sshkeys_core module
+class { 'sshkeys_core':
+  client_package_name => 'openssh-client',
 }
+
+# Configure SSH client
+ssh::client::config { 'default':
+  ensure             => present,
+  user               => $::ssh::params::user,
+  path               => $::ssh::params::config_file,
+  priority           => 10,
+  host               => '<remote_server>',
+  identity_file      => '/home/<your_username>/.ssh/school',
+  password_auth      => 'no',
+}
+
