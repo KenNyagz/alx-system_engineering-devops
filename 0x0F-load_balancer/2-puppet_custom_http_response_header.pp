d5d7d3ab2f80c951dae9f2d2411b181ae2cb569e
KenNyagz <kennyagz681@gmail.com>
2024-03-05 20:01:55 +0300

    haproxy load bal puppet confign

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,17 @@
+# Configuring haproxy load balancer
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/etc/nginx/conf.d/custom_http_header.conf':
+  ensure  => present,
+  content => "server_tokens off;\nadd_header X-Served_By ${::hostname};\n",
+  notify  => service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enanble => true,
+  subscribe => File['/etc/nginx/conf.d/custom_http_header.conf'],
+}
