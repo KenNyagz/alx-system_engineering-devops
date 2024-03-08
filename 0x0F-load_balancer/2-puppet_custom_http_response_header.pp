b4ea12a828cc05bfad3a9abbdca63fe6f133ee46
KenNyagz <kennyagz681@gmail.com>
2024-03-08 11:32:05 +0300

    An aliased Commit to save time

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,7 +4,7 @@ class nginx_custom_header {
   # Ensure Nginx is installed
   package { 'nginx':
     ensure => installed,
-  }
+  }->
 
   # Manage the Nginx configuration to include the custom header
   file { '/etc/nginx/conf.d/custom_header.conf':
@@ -12,7 +12,7 @@ class nginx_custom_header {
     content => epp('nginx_custom_header/custom_header.epp'),
     require => Package['nginx'],
     notify  => Service['nginx'],
-  }
+  }->
 
   # Ensure the Nginx service is running and enabled
   service { 'nginx':
