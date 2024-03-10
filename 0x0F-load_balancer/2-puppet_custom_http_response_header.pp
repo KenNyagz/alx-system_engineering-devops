b0463ce0062a5a71dded974db14bc7f693720bd0
KenNyagz <kennyagz681@gmail.com>
2024-03-11 00:20:34 +0300

    An aliased Commit to save time

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,24 @@
 # Configuring haproxy load balancer
 
-#class nginx_custom_header {
-  # Ensure Nginx is installed
-  package { 'nginx':
-    ensure => installed,
-  }->
+# Install nginx package
+package { 'nginx':
+  ensure => installed,
+}
 
-  # Manage the Nginx configuration to include the custom header
-  file { '/etc/nginx/conf.d/custom_header.conf':
-    ensure  => file,
-    content => epp('nginx_custom_header/custom_header.epp'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }->
+# Define the custom HTTP header
+$server_hostname = $facts['hostname']
+$custom_header = "add_header X-Served-By '${server_hostname}';"
 
-  # Ensure the Nginx service is running and enabled
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    require => File['/etc/nginx/conf.d/custom_header.conf'],
-  }
-#}
-#include nginx_custom_header
+# Configure nginx
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('path/to/nginx_template.erb'), # Path to a template file for nginx configuration
+  notify  => Service['nginx'],
+}
+
+# Ensure nginx service is running and enabled
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
