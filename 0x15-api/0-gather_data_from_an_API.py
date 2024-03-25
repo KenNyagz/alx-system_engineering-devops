7f4933c38b3afeea58c97df97714ed69e4f34d45
KenNyagz <kennyagz681@gmail.com>
2024-03-25 21:00:03 +0300

    Gathering data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -28,7 +28,7 @@ def get_employee_progress(employee_id):
     done_tasks = [task for task in todo_list if task['completed']]
     num_done_tasks = len(done_tasks)
     print(f"Employee {employee_name} is done with tasks\
- ({num_done_tasks}/{total_tasks}):")
+({num_done_tasks}/{total_tasks}):")
 
     for task in done_tasks:
         print(f"\t {task['title']}")
