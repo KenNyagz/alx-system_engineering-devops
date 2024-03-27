ed891f1ba860acf4971b39330f72e1e37cedd1f4
KenNyagz <kennyagz681@gmail.com>
2024-03-27 21:29:30 +0300

    from api to csv, with QUOTE_ALL

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,22 +5,23 @@ import requests
 import sys
 
 
-userID = sys.argv[1]
+if __name__ == '__main__':
+    userID = sys.argv[1]
 
-api = 'https://jsonplaceholder.typicode.com/'
+    api = 'https://jsonplaceholder.typicode.com/'
 
-user_url = api + 'users/' + userID
-resp = requests.get(user_url)
-user_data = resp.json()
-username = user_data['username']
+    user_url = api + 'users/' + userID
+    resp = requests.get(user_url)
+    user_data = resp.json()
+    username = user_data['username']
 
-todo_url = api + 'todos?userId=' + userID
-response = requests.get(todo_url)
+    todo_url = api + 'todos?userId=' + userID
+    response = requests.get(todo_url)
 
-tasks = response.json()
+    tasks = response.json()
 
-filename = f'{userID}.csv'
-with open(filename, 'w') as f:
-    writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-    for task in tasks:
-        writer.writerow([userID, username, task['completed'], task['title']])
+    filename = f'{userID}.csv'
+    with open(filename, 'w') as f:
+        wtr = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            wtr.writerow([userID, username, task['completed'], task['title']])
