6dc9af616f1fcbbed426dd24c08d33f784554557
KenNyagz <kennyagz681@gmail.com>
2024-03-27 21:24:55 +0300

    from api to csv, with QUOTE_ALL

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,47 +1,25 @@
 #!/usr/bin/python3
-'''export data in the CSV format
-'''
+import csv
 import requests
 import sys
-import csv
 
 
-def get_employee_progress(employee_id):
-    '''Get's an employees to-do list progress'''
-    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    response = requests.get(employee_url)
-    if response.status_code != 200:
-        print('Error loading data.')
-        return
+userID = sys.argv[1]
 
-    employee_data = response.json()
-    employee_name = employee_data.get('name')
+api = 'https://jsonplaceholder.typicode.com/'
 
-    todo_list_url = f"https\
-://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    response = requests.get(todo_list_url)
-    if response.status_code != 200:
-        print("Error: TODO list not found")
-        return
+user_url = api + 'users/' + userID
+resp = requests.get(user_url)
+user_data = resp.json()
+username = user_data['username']
 
-    todo_list = response.json()
+todo_url = api + 'todos?userId=' + userID
+response = requests.get(todo_url)
 
-    filename = f"{employee_id}.csv"
+tasks = response.json()
 
-    with open(filename, mode='w', newline='') as fle:
-        writer = csv.writer(fle, quoting=csv.QUOTE_ALL)
-
-        for task in todo_list:
-            writer.writerow([employee_id, employee_name,
-                            task['completed'], task['title']])
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage:{} <employee_id>".format(sys.argv[0]))
-    else:
-        try:
-            employee_id = int(sys.argv[1])
-            get_employee_progress(employee_id)
-        except ValueError:
-            print("Error:ID not an integer")
+filename = f'{userID}.csv'
+with open(filename, 'w') as f:
+    writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+    for task in tasks:
+        writer.writerow([userID, username, task['completed'], task['title']])
