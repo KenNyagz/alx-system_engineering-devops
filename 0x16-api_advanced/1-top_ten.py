04031800e16434cd6ff7d282622ed9f8b82d9a64
KenNyagz <kennyagz681@gmail.com>
2024-04-09 18:43:56 +0300

    Filter top ten hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,3 +2,20 @@
 '''queries the Reddit API and prints the titles of the first 10 hot 
 posts listed for a given subreddit'''
 import requests
+
+
+def top_ten(subreddit):
+    '''Gets top ten hot posts from a subreddit'''
+    uri = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    headers = {'User-agent': 'Myself'}
+
+    resp = requests.get(uri, headers=headers, params={'limit': 10})
+    if resp.status_code == 200:
+        data = resp.json()
+        titles = [post['data']['title'] for post in data['data']['children']]
+
+        for title in titles:
+            print("{}".format(title))
+    else:
+        print("None")
