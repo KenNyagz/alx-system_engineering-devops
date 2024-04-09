5f75954def44b8179861385dd1048d410665be5d
KenNyagz <kennyagz681@gmail.com>
2024-04-09 17:47:08 +0300

    Querying an api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Returns the number of subscribers of a given subreddit'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''Retrieves number of subscribers of a subreddit community'''
+    api_url = "https://www.reddit.com"
+    endpoint = f"/r/{subreddit}/about.json"
+    headers = {'User-agent': 'Mozilla/5.0'}
+
+    url = api_url + endpoint
+    resp = requests.get(url, headers=headers)
+
+    if resp.status_code == 200:
+        data = resp.json()
+        return data['data']['subscribers']
+    else:
+        return 0
