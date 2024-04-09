4ce08b9d290cd1f21be354c2c6d401c9dd87a0e2
KenNyagz <kennyagz681@gmail.com>
2024-04-09 22:53:12 +0300

    Recursive api call

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Recursively queries reddit api returning a list of all hot articles of a 
+subreddit'''
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    '''return a list of all hot articless of a subreddit'''
+    # set base  case
+    if hot_list is None:
+        hot_list = []
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': "Must"}
+    params = {'limit': 80, 'after': after}
+
+    resp = requests.get(url, headers=headers, params=params)
+    if resp.status_code == 200:
+        data = resp.json()
+        posts = data['data']['children']
+        if not posts:
+            return None
+        for post in posts:
+            hot_list.append(post['data']['title'])
+
+        after = data['data'].get('after')
+        if after:
+                recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+    else:
+        return None
