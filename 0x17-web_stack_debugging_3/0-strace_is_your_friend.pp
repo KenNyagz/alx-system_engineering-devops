1c4b2e5629af057656b03c350f3f2066b8e56bf3
KenNyagz <kennyagz681@gmail.com>
2024-04-11 00:07:59 +0300

    debug apche with strace, automate with puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,23 +1,11 @@
-# Debug apache
-$apache_pid = pgrep -f httpd
+# Ensure Apache service is running
+service { 'apache2':
+  ensure => 'running',
+}
 
-class apache {
-    package { 'apache2':
-        ensure => installed,
-    }
-
-    service { 'apache2':
-        ensure  => running,
-        enable  => true,
-        require => Package['apache2'],
-    }
-
-    # You can manage Apache configuration files here
-    # For example, you might ensure the correct settings are in httpd.conf
-    file { '/etc/apache2/httpd.conf':
-        ensure  => file,
-        content => template('apache/httpd.conf.erb'),
-        require => Package['apache2'],
-        notify  => Service['apache2'],
-    }
+# Define file resource to manage Apache configuration file
+file { '/etc/apache2/apache2.conf':
+  ensure  => file,
+  content => template('my_module/apache2.conf.erb'),
+  notify  => Service['apache2'], # Restart Apache if the config changes
 }
