ca5df24fe533ad82438de3acf339f67fa1d7cc34
KenNyagz <kennyagz681@gmail.com>
2024-04-11 00:16:51 +0300

    An aliased Commit to save time

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,17 @@
-# Ensure Apache service is running
-service { 'apache2':
-  ensure => 'running',
+class apache_fix {
+  file { '/etc/httpd/conf/httpd.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('apache/httpd.conf.erb'),
+    notify  => Service['httpd'],
+  }
+
+  service { 'httpd':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/httpd/conf/httpd.conf'],
+  }
 }
 
-# Define file resource to manage Apache configuration file
-file { '/etc/apache2/apache2.conf':
-  ensure  => file,
-  content => template('my_module/apache2.conf.erb'),
-  notify  => Service['apache2'], # Restart Apache if the config changes
-}
