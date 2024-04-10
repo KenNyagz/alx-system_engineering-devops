7ad46ff700b35b385dbcd3c74e6de2d415ca219d
KenNyagz <kennyagz681@gmail.com>
2024-04-10 23:53:46 +0300

    wah

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,35 +1,20 @@
-# apache debugging
+# Debug apache
+$apache_pid = pgrep -f httpd
+
 module apache_strace {
-  class apache_strace (
-    $strace_options = '-p 8181 -o /tmp/strace.log -s 200 -ff'
-  ) {
-    # This class installs and configures strace to monitor Apache processes
-
-    # The strace options to use when monitoring Apache processes
-    $strace_options
-
-    # Ensure that strace is installed
-    package { 'strace':
-      ensure => present,
-    }
-
-    # Create a script to run strace on Apache processes
+  class apache_strace {
     file { '/usr/local/bin/strace_apache.sh':
-      ensure  => file,
-      mode    => '0755',
-      content => template('apache_strace/strace_apache.sh.erb'),
+      ensure => file,
+      mode   => '0755',
+      source => 'puppet:///modules/apache_strace/strace_apache.sh',
     }
 
-    # Schedule a cron job to run the script periodically
     cron { 'strace_apache':
       ensure  => present,
       user    => 'root',
       command => '/usr/local/bin/strace_apache.sh',
-      minute  => '0',
       hour    => '2',
+      minute  => '0',
     }
   }
-
-  # Ensure that the apache_strace class is always loaded
-  Class['apache_strace'] -> File['/etc/cron.d/strace_apache']
 }
