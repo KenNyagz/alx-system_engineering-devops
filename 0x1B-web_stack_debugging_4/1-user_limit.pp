ecc859b6bc3a1faf3c80e16f0b19ccb90ef29ab4
KenNyagz <kennyagz681@gmail.com>
2024-04-18 21:45:18 +0300

    fix user's file limits

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,11 @@
 # ppt manifest to fix a user's file permission limit
 
-exec { 'fix_limit_hbton_user':
-  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
-}
 exec { 'increase_soft_file':
-  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
+}
+
+exec { 'fix_limit_hbton_user':
+  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
 }
