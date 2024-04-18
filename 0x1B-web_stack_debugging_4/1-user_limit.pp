bfd917209977b49673974e0a814669887b2cac55
KenNyagz <kennyagz681@gmail.com>
2024-04-18 18:46:20 +0300

    fix user's file limits

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
-# ppt manifest to add user and give root permission
+# ppt manifest to fix a user's file permission limit
 
 exec { 'fix_user_limit':
   command => 'sed -i "/holberton/s/[0-9]/&00/" /etc/security/limits.conf',
-  path    => ['/usr/sbin', '/usr/local/bin', '/usr/local/sbin']
+  path    => ['/usr/sbin', '/usr/local/bin', '/usr/local/sbin'],
   # path    => '/usr/sbin/:/usr/local/bin/:/usr/local/sbin/',
 }
