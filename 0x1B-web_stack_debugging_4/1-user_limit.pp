06a7a1071f3c64a52e522fcee874ec536aa7f2be
KenNyagz <kennyagz681@gmail.com>
2024-04-18 18:42:55 +0300

    fix user's file limits

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,5 +2,5 @@
 
 exec { 'fix_user_limit':
   command => 'sed -i "/holberton/s/[0-9]/&00/" /etc/security/limits.conf',
-  path    => '/usr/sbin/:/usr/local/bin/:/usr/local/sbin/'
+  path    => '/usr/sbin/:/usr/local/bin/:/usr/local/sbin/',
 }
