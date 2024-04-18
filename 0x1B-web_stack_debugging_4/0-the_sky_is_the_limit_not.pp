7550fca986796f18b677552e782536e441d03cc0
KenNyagz <kennyagz681@gmail.com>
2024-04-18 15:05:47 +0300

    extend file descriptor limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # ppt manifest to increase the file descriptior limit of ubuntu server
 
 exec { 'fd_limmit_change':
-  path    => '/usr/bin, /usr/sbin, /usr/local/bin',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin',
   command => sed 's/15/4096/' /etc/default/nginx,
 } ->
 
